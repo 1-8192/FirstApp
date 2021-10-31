7aac2e8e6fbae58877c1368621b7883deb9f8623
Alessandro Allegranzi <allegranzia@gmail.com>
2021-10-31 11:30:55 -0400

    added styles

--- Home.js
+++ Home.js
@@ -1,10 +1,47 @@
 import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
+
+const introText = "this is just nothing";
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 20
+  },
+  textContainer: {
+    textAlign: "center",
+    paddingTop: 10
+  },
+  logo: {
+    height: 100,
+    width: 150
+  },
+  title: {
+    fontFamily: "ZenKakuGothicAntique-Regular"
+  },
+  subtitle: {
+    paddingTop: 5,
+    fontFamily: "ZenKakuGothicAntique-Regular"
+  },
+  content: {
+    fontFamily: "ZenKakuGotchicAntique-Light",
+    fintWeight: "300"
+  }
+});
 
 const Home = props => {
   return (
-    <View style={{ flexDirection: "column" }}>
-      <Image source={require('./images/gwyn.jpg')}/>
+    <View style={styles.container}>
+      <Image source={require("./images/gwyn.jpg")} />
+      <Text style={styles.title}>Welcome to my first react native app </Text>
+      <Text style={styles.subtitle}>
+        {props.userName}
+      </Text>
+      <View style={style.textContainer}>
+        <Text style={styles.content}>
+          {introText}
+        </Text>
+      </View>
     </View>
   );
 };
